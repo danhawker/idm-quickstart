e28a8e6a66dc7baa3d1d86dae7836d676e79d4f7
Josh Preston <the@mrjoshuap.com>
2018-10-26 00:29:23 -0400

    move config out of provisioning, it wasn’t working properly

--- provision.sh
+++ provision.sh
@@ -28,51 +28,18 @@ yum -y install nfs-utils NetworkManager
 systemctl enable NetworkManager
 systemctl status NetworkManager || systemctl start NetworkManager
 
-# use a default of example.test if not specified
-DOMAIN=${DOMAIN:-example.test}
-
-# specify some IPs for our IPs
-IP_CIDR="172.17.0.0/24"
-IP_IDM_1="172.17.0.2"
-IP_IDM_2="172.17.0.3"
-IP_NFS="172.17.0.4"
-IP_CLIENT7_1="172.17.0.9"
-IP_CLIENT6_1="172.17.0.19"
-
-# set up some dns stuffs
-DNS_REVERSE_ZONE="0.17.172.in-addr.arpa."
-DNS_FORWARDERS="8.8.8.8 8.8.4.4"
-
-# set a max of fake users (up to 50000)
-MAX_FAKE_USERS=100
-
 # make sure all hosts can be found
 echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts
 echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/hosts
 echo "${IP_IDM_1}  idm-1.${DOMAIN} idm-1" >> /etc/hosts
-echo "${IP_IDM_2}  idm-2.${DOMAIN} idm-2" >> /etc/hosts
 echo "${IP_NFS}  nfs.${DOMAIN} nfs" >> /etc/hosts
 echo "${IP_CLIENT7_1}  client7-1.${DOMAIN} client7-1" >> /etc/hosts
 echo "${IP_CLIENT6_1}  client6-1.${DOMAIN} client6-1" >> /etc/hosts
 
 # create a new set of passwords to be used for our installation
 if [ ! -f /vagrant/secure.env ]; then
-  echo "Generating new passwords for use with our setup..."
-  echo DOMAIN=\"${DOMAIN}\" >> /vagrant/secure.env
-  echo REALM=\"$(echo ${DOMAIN} | tr [a-z] [A-Z])\" >> /vagrant/secure.env
-  echo NETWORK_DEVICE=\"${NETWORK_DEVICE}\" >> /vagrant/secure.env
-  echo IP_CIDR=\"${IP_CIDR}\" >> /vagrant/secure.env
-  echo IP_IDM_1=\""${IP_IDM_1}"\" >> /vagrant/secure.env
-  echo IP_IDM_2=\""${IP_IDM_2}"\" >> /vagrant/secure.env
-  echo IP_NFS=\""${IP_NFS}"\" >> /vagrant/secure.env
-  echo IP_CLIENT7_1=\""${IP_CLIENT7_1}"\" >> /vagrant/secure.env
-  echo IP_CLIENT6_1=\""${IP_CLIENT6_1}"\" >> /vagrant/secure.env
-  echo DNS_REVERSE_ZONE=\""${DNS_REVERSE_ZONE}"\" >> /vagrant/secure.env
-  echo DNS_FORWARDERS=\""${DNS_FORWARDERS}"\" >> /vagrant/secure.env
-  echo DM_PASSWORD=\""$(openssl rand -base64 16 | tr -dc [:alnum:])"\" >> /vagrant/secure.env
-  echo ADMIN_PASSWORD=\""$(openssl rand -base64 16 | tr -dc [:alnum:])"\" >> /vagrant/secure.env
-  echo MAX_FAKE_USERS=${MAX_FAKE_USERS} >> /vagrant/secure.env
-  echo "Passwords are stored in secure.inc"
+  echo "Error, you didn't generate configurations or passwords for use with our setup... modify and run config.sh from the host"
+  exit -1
 fi
 
 # Create our home directories
